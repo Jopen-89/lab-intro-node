b7fdbc2addb64b0ed8bdc8245dd5276461e15924
jopen <solimonra@gmail.com>
2025-09-01 12:38:18 +0200

    Ejercicio de intro a Node.js completado, tests de Mocha superados

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => {
+    if (a < b) {return -1}
+    if (a > b) {return + 1}
+    if (a === b) {return 0}
+  }) 
+  this.length = this.items.length
+  }
 
-  get(pos) {}
+  // if (pos <= this.items.length - 1)
+  get(pos) {
+    if (pos >= 0 && pos < this.items.length) {
+      return this.items[pos];
+    } else {throw new Error ("OutOfBounds")}
+  }
 
-  max() {}
 
-  min() {}
+  max() {
+    if (this.items.length > 0) {
+      const maxNumber = this.items[this.items.length -1];
+      return maxNumber; 
+    } else {  throw new Error("EmptySortedList")}
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length > 0) {
+    const minNumber = this.items[0];
+    return minNumber;
+    } else {throw new Error("EmptySortedList")}
 
-  avg() {}
+  }
+  //quiero utilizar reduce array.reduce ((acc, actual => return acc + actual, 0)
+  sum() {
+    const sumArray = this.items.reduce((accu, actual) => {
+    return accu + actual; }, 0);
+
+    return sumArray
+  };
+
+  avg() {
+    if (this.items.length > 0) {
+    const avg = this.sum()/this.items.length
+    return avg 
+    } else { throw new Error("EmptySortedList")
+  } 
+}
 }
 
 module.exports = SortedList;
